0743fb2b3053772c8b99f3763737f175ac50c0ee
Avanish Singh <79073722+aaviix@users.noreply.github.com>
2023-11-29 20:47:11 +0100

    Add files via upload

---
+++ frontend-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatbotComponent from './components/ChatbotComponent';
+
+const App = () => {
+    return (
+        <div>
+            <ChatbotComponent />
+        </div>
+    );
+};
+
+export default App;
---
+++ frontend-react/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chatbot from "./components/ChatbotComponent";
+
+ReactDOM.render(
+    <Chatbot/>,
+    document.getElementById('root')
+);
